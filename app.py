b25eb42750827f894e9b0ab2e61d061ac8ba46aa
peter-icanio <134062976+peter-icanio@users.noreply.github.com>
2023-06-10 12:02:03 +0530

    5 commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Dummy data - books
 books = [
     {"id": 1, "title  icanio": "Book 1", "author": "Author 1"},
-    {"id": 2, "title": "Book 2", "author": "Author 2"},
+    {"id": 2, "title icanioooo": "Book 2", "author": "Author 2"},
 ]
 
 # GET - Retrieve all books
