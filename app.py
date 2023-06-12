7cf38df9cba69fcf78ab712cf3a75f21d561db0e
peter-icanio <134062976+peter-icanio@users.noreply.github.com>
2023-06-12 17:06:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 # Dummy data - books
 books = [
-    {"id": 1, "title  icanio": "Book 1", "author": "Author 1"},
+    {"id": 1, "title  icaniooooooooo": "Book 1", "author": "Author 1"},
     {"id": 2, "title icanioooo": "Book 2", "author": "Author 2"},
 ]
 
